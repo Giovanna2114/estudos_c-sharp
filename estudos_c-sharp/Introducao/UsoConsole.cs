07f1d0e0fc3dfc240b15f7eb0af50cfb38ff2700
Giovanna Teixeira <giovanna.teixeira563@gmail.com>
2025-03-11 23:13:14 -0300

    Preparação do ambiente e primeiro contato com a linguagem.

---
+++ estudos_c-sharp/Introducao/UsoConsole.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudos_c_sharp.Introducao
+{
+
+    class UsoConsole
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            // Impressão no console output:
+
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Congratulations!");
+            Console.Write(" ");
+            Console.Write("You wrote your first lines of code!");
+
+            //Console.Write() - Escreve na mesma linha.
+            //Console.WriteLine() - Insere uma quebra de linha ao final.
+        }
+    }
+}
---
+++ estudos_c-sharp/Introducao/Var_and_Const_Values.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudos_c_sharp.Introducao
+{
+    class Var_and_Const_Values
+    {
+
+        static void Main(string[] args)
+        {
+            // Char - Um único caractere alfanumérico:
+            Console.WriteLine('G');
+
+            // String - Grupo de caracteres:
+            Console.WriteLine("Grupo");
+
+            // Int - Números inteiros:
+            Console.WriteLine(123);
+
+            // Float, double, decimal - Números decimais:
+            Console.WriteLine(0.233F); //Sufixo F/f - Número float
+            Console.WriteLine(2.6354); //Sem sufixo - Número double
+            Console.WriteLine(2.150m); //Sufixo M/m - Número decimal
+
+            //Bool - Booleanos (Verdadeiro ou Falso):
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+        }
+    }
+}
