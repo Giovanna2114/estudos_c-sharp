50e8460f6bf23721c21e119834933bdfca2651af
Giovanna Teixeira <giovanna.teixeira563@gmail.com>
2025-03-15 23:19:30 -0300

    Compreendendo sintaxe básica #1.

---
+++ estudos_c-sharp/Introducao/FormatacaoCaracteres.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudos_c_sharp.Introducao
+{
+    class FormatacaoCaracteres
+    {
+
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello\nWorld"); // Adiciona uma quebra de linha.
+            Console.WriteLine("Hello\tWorld"); // Adiciona um espaçamento entre as palavras.
+
+            Console.WriteLine("Hello \"World\""); // O caractere de escape '\' permite adicionar aspas ao texto. 
+
+            Console.WriteLine(@"    c:source\repos
+                    (this is where code goes)"); // O caractere '@' adicionado antes do texto faz com que tudo o que seja
+                                                 // escrito seja interpretado literalmente, sem necessidade do uso de '\'.
+
+            // To generate Japanese invoices:
+            // Nihon no seikyū-sho o seisei suru ni wa:
+            Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+            // Os caracteres unicode '\u' são utilizados para adicionar simbolos.
+
+            // User command to run an application
+            Console.WriteLine(@"c:\invoices\app.exe -j");
+
+            // Concatenação e interpolação:
+            string firstName = "Bob";
+            string greeting = "Hello";
+
+            // Concatenando variáveis:
+            Console.WriteLine(greeting + " " + firstName + "!");
+
+            // Interpolando variáveis:
+            Console.WriteLine($"{greeting} {firstName}");
+
+
+            // Combinando literais textuais e interpolação de variáveis:
+            string projectName = "First-Project";
+            Console.WriteLine($@"C:\Output\{projectName}\Data");
+
+            
+
+        }
+    }
+}
--- estudos_c-sharp/Introducao/Var_and_Const_Values.cs
+++ estudos_c-sharp/Introducao/Var_and_Const_Values.cs
@@ -14,20 +14,44 @@ namespace estudos_c_sharp.Introducao
             // Char - Um único caractere alfanumérico:
             Console.WriteLine('G');
 
+            char userOption = 'S'; // Declaração e atribuição de valor à variável
+            Console.WriteLine(userOption);
+
             // String - Grupo de caracteres:
             Console.WriteLine("Grupo");
 
+            string firstName = "Clara"; // Declaração e atribuição de valor à variável
+            Console.WriteLine(firstName);
+
             // Int - Números inteiros:
-            Console.WriteLine(123);
+            Console.WriteLine(123); 
+
+            int gameScore = 93; // Declaração e atribuição de valor à variável
+            Console.WriteLine(gameScore); 
 
             // Float, double, decimal - Números decimais:
             Console.WriteLine(0.233F); //Sufixo F/f - Número float
             Console.WriteLine(2.6354); //Sem sufixo - Número double
             Console.WriteLine(2.150m); //Sufixo M/m - Número decimal
 
+            double particlesPerMillion = 1.6; // Declaração e atribuição de valor à variável
+            Console.WriteLine(particlesPerMillion);
+
             //Bool - Booleanos (Verdadeiro ou Falso):
             Console.WriteLine(true);
             Console.WriteLine(false);
+
+            bool processedCustomer = false; // Declaração e atribuição de valor à variável
+            Console.WriteLine(processedCustomer);
+
+
+            // Variáveis locais de tipo implicito:
+            var implicitoStr = "ABCDE";
+
+            var implicitoInt = 12345;
+
+            Console.WriteLine("String implicita = " + implicitoStr + "\nInt implicito = " + implicitoInt);
+
         }
     }
 }
